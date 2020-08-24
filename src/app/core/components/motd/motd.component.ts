1ddec9647fc79e09753f733de45aeca09b2be75b
Phil Larragoite <phil@dukecitydigital.com>
2020-08-24 06:08:50 -0600

    component shells

---
+++ src/app/core/components/motd/motd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motd',
+  templateUrl: './motd.component.html',
+  styleUrls: ['./motd.component.scss']
+})
+export class MotdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/core/components/omni-table/omni-table.component.ts
+++ src/app/core/components/omni-table/omni-table.component.ts
@@ -31,6 +31,7 @@ import {
   distinctUntilChanged,
   tap,
 } from "rxjs/operators";
+import { element } from "protractor";
 @Component({
   selector: "app-omni-table",
   templateUrl: "./omni-table.component.html",
@@ -38,11 +39,17 @@ import {
 })
 export class OmniTableComponent implements OnInit {
   @Input() columns: any = ["id"];
+  @Input() objectColumns: any[] = [
+    { name: "ya", selectable: true, editable: true, inputType: "text" },
+  ];
+
   @Input() title: string = "Omni Table";
 
-  @Input() selectableRows: any;
+  //can rows be selected generally
+  @Input() selectableRows: boolean;
   dataSource: OmniDataSource;
   resultsLength = 0;
+  displayedColumns: any[] = this.objectColumns.map((col) => col.name);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
@@ -58,9 +65,41 @@ export class OmniTableComponent implements OnInit {
       "",
       "",
       "asc",
-      0,
-      30
+      1,
+      10
     );
+    window.setTimeout(
+      () => (this.resultsLength = this.dataSource.resultsLength),
+      750
+    );
+    this.createObjectColumns();
+  }
+
+  onPaginateChange(event) {
+    console.log(event);
+    this.paginator.pageIndex = event.pageIndex;
+  }
+
+  /**
+   * Transform input columns into objects if string
+   * @param columns
+   */
+  createObjectColumns() {
+    var objectColumns;
+    var displayColumns = [];
+    var returnColumns = [];
+    if (typeof this.columns[0] == "string") {
+      this.columns.forEach((element) => {
+        displayColumns.push(element);
+        var item = {
+          name: element,
+        };
+        returnColumns.push(item);
+      });
+      // this.objectColumns.push({ name: element });
+    }
+    this.objectColumns = returnColumns;
+    this.displayedColumns = this.objectColumns.map((col) => col.name);
   }
 
   ngAfterViewInit(): void {
@@ -72,28 +111,34 @@ export class OmniTableComponent implements OnInit {
         debounceTime(150),
         distinctUntilChanged(),
         tap(() => {
-          this.paginator.pageIndex = 0;
+          this.paginator.pageIndex = 1;
           this.loadOmniPage();
+          console.log("datas", this.dataSource);
+          // this.resultsLength = this.dataSource.resultsLength;
         })
       )
       .subscribe();
 
     // reset the paginator after sorting
     this.sort.sortChange.subscribe(() => {
-      // console.log(this.sort);
-      this.paginator.pageIndex = 0;
+      console.log(this.sort);
+      this.paginator.pageIndex = 1;
+      // this.resultsLength = this.dataSource.resultsLength;
     });
 
     // on sort or paginate events, load a new page
     merge(this.sort.sortChange, this.paginator.page)
       .pipe(
         tap(() => {
+          console.log(this.paginator);
           this.loadOmniPage();
-          this.resultsLength = this.dataSource.resultsLength;
+
+          // this.resultsLength = this.dataSource.resultsLength;
         })
       )
       .subscribe();
   }
+
   loadOmniPage() {
     this.dataSource.loadomni(
       this.title.toLowerCase(),
@@ -141,6 +186,7 @@ export class OmniDataSource implements DataSource<any> {
         finalize(() => this.loadingSubject.next(false))
       )
       .subscribe((logs: any) => {
+        console.log(logs);
         this.resultsLength = logs.total;
         this.omniSubject.next(logs.data);
       });
