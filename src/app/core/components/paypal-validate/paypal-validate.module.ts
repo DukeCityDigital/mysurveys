cb10a5e236582e92aa180377590eb62e37c5c9e1
Phil Larragoite <phil@dukecitydigital.com>
2020-12-16 10:04:21 -0700

    paypal validation interface

--- src/app/core/components/paypal-validate/paypal-validate.module.ts
+++ src/app/core/components/paypal-validate/paypal-validate.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PaypalValidateComponent } from "./paypal-validate.component";
 import { MaterialBaseModule } from "../material-base/material-base.module";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [PaypalValidateComponent],
-  imports: [CommonModule, MaterialBaseModule],
+  imports: [
+    CommonModule,
+    MaterialBaseModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [PaypalValidateComponent],
 })
 export class PaypalValidateModule {}
