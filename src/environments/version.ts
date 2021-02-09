f97b7bfa32c5a9757f9072a3dd7f0e44e1b375e3
Phil Larragoite <phil@dukecitydigital.com>
2021-02-09 00:07:17 -0700

    version

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -8,7 +8,7 @@ export const VERSION = {
   semver: null,
   suffix: "8f8ba28-dirty",
   semverString: null,
-  version: "2.0204.1a",
+  version: "2.0209.1a",
   description: "beta",
 };
 /* tslint:enable */
