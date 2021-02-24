85c6c0c3f290d6abc150a429a530bceb5a64096d
Phil Larragoite <phil@dukecitydigital.com>
2021-02-24 09:48:10 -0700

    allow non-logged in to questionnaire

--- src/app/core/helpers/auth.guard.ts
+++ src/app/core/helpers/auth.guard.ts
@@ -79,7 +79,8 @@ export class StepGuard implements CanActivate {
       }
       return true;
     }
-    if (state.url == "/questionnaire") {
+    console.log(state.url);
+    if (state.url.indexOf("/questionnaire") > -1) {
       return true;
     }
     // not logged in so redirect to login page with the return url
