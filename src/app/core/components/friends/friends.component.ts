37e085bdece8c3292ad572e6c8a8f87f84c2c744
unknown <phil@dukecitydigital.com>
2021-06-16 17:47:15 -0600

    invitation form frontend

--- src/app/core/components/friends/friends.component.ts
+++ src/app/core/components/friends/friends.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { AlertService } from "../_alert";
 import { ParticipantService } from "@app/core/services/participant.service";
 import { AuthService } from "@app/core/services/auth.service";
+import { tap } from "rxjs/operators";
 
 @Component({
   selector: "app-friends",
@@ -21,6 +22,8 @@ export class FriendsComponent implements OnInit {
   ) {
     this.friendForm = this.createFriendform();
     this.authenticationService.user.subscribe((x) => (this.user = x));
+    this.profileForm = this.createProfileForm();
+
   }
 
   ngAfterViewInit(): void {
@@ -30,26 +33,68 @@ export class FriendsComponent implements OnInit {
   }
 
   getMe() {
-    this.participantService.profile().subscribe((data: any) => {
-      this.user = data.data;
-      console.log(this.user);
-      if (this.user.friends.length > 0) {
-        localStorage.setItem("step", "");
+    this.participantService.profile()
+      .pipe(tap((user: any) => this.profileForm.patchValue(user.data)))
 
-        confirm(
-          "Thank you for inviting friends!  You can now receive survey invitations."
-        );
-      }
-    });
+      .subscribe((data: any) => {
+        this.user = data.data;
+        console.log(this.user);
+
+        if (this.user.friends.length > 0) {
+          localStorage.setItem("step", "");
+
+          confirm(
+            "Thank you for inviting friends!  You can now receive survey invitations."
+          );
+        }
+      });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   createFriendform(): FormGroup {
     return this.formBuilder.group({
       email: new FormControl(""),
       custom_message: new FormControl(""),
+      nickname: new FormControl("", [Validators.required]),
+
     });
+  }
+  profileForm: FormGroup;
+
+
+  createProfileForm(): FormGroup {
+    return this.formBuilder.group({
+      nickname: new FormControl(""),
+
+    });
+  }
+
+  onSubmitProfileChange() {
+    if (!this.profileForm) {
+      this.alertService.error(
+        "You must fill out the profile change form first"
+      );
+    }
+    this.participantService.update(this.profileForm.value).subscribe((data) => {
+      this.getProfile();
+    });
+  }
+  participant: any;
+
+  getProfile() {
+    this.participantService
+      .profile()
+      .pipe(tap((user: any) => this.profileForm.patchValue(user.data)))
+      .subscribe((data: any) => {
+        this.participant = data.data;
+        if (
+          !this.participant.changed_pw ||
+          this.participant.changed_pw == null
+        ) {
+        }
+        this.alertService.success(data.message, { autoClose: true });
+      });
   }
 
   public inviteFriend(post) {
@@ -82,6 +127,8 @@ export class FriendsComponent implements OnInit {
     this.friendForm.setValue({ email: email, custom_message: "" });
     let post = {
       email: this.friendForm.value.email,
+      nickname: this.friendForm.value.nickname,
+
       invite: true,
       remind: true,
       custom_message: this.friendForm.value.custom_message,
@@ -91,13 +138,15 @@ export class FriendsComponent implements OnInit {
   onSubmitFriendInvite() {
     let post = {
       email: this.friendForm.value.email,
+      nickname: this.friendForm.value.nickname,
+
       invite: true,
       custom_message: this.friendForm.value.custom_message,
     };
     if (this.friendForm.value.custom_message == "") {
       delete post.custom_message;
     }
-
+    console.log(post);
     this.inviteFriend(post);
     this.friendForm.reset();
   }
